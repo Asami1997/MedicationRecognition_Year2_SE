0239de714e949f54ac9181576d5be410114296d7
znixonz <znixonz@yahoo.com>
2017-11-09 14:43:17 +0800

    updated Database

---
+++ app/src/main/java/com/project/year2/medicationrecognition/DatabaseHelper.java
@@ -0,0 +1,86 @@
+package com.project.year2.medicationrecognition;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Nixon on 5/11/2017.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "medication.db";
+    public static final String TABLE_NAME = "medication_table";
+    public static final String COLUMN_1 = "ID";
+    public static final String COLUMN_2 = "NAME";
+    public static final String COLUMN_3 = "INGREDIENT";
+    public static final String COLUMN_4 = "INVENTORY";
+
+    public DatabaseHelper(Context context)
+    {
+        super(context, DATABASE_NAME, null, 1);
+    }
+    private static final String Create_Table_medication = "CREATE TABLE " + TABLE_NAME + "(" + COLUMN_1 + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_2 + " TEXT," + COLUMN_3 + " TEXT," + COLUMN_4 +" INTEGER);";
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(Create_Table_medication);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS" + TABLE_NAME);
+
+        onCreate(db);
+    }
+
+    public boolean insertData(String name,String ingredient,String inventory)
+    {
+
+        //this command is used to view database Log.i("name","nixon");
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_2,name);
+        contentValues.put(COLUMN_3,ingredient);
+        contentValues.put(COLUMN_4,inventory);
+
+        long result = db.insert(TABLE_NAME,null,contentValues);
+        if (result == -1)
+            return false;
+        else
+            return true;
+    }
+
+
+    public Cursor displayData()
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("SELECT * FROM " + TABLE_NAME,null);
+        return res;
+    }
+
+    public boolean updateData(String id,String name,String ingredient,String inventory )
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_1,id);
+        contentValues.put(COLUMN_2,name);
+        contentValues.put(COLUMN_3,ingredient);
+        contentValues.put(COLUMN_4,inventory);
+
+        db.update(TABLE_NAME,contentValues,"ID = ?", new String[] { id });
+        return true;
+
+    }
+
+    public Integer deleteData(String id)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+         return db.delete(TABLE_NAME,"ID = ?",new String[] {id});
+
+    }
+
+}
--- app/src/main/java/com/project/year2/medicationrecognition/MainActivity.java
+++ app/src/main/java/com/project/year2/medicationrecognition/MainActivity.java
@@ -1,144 +1,141 @@
 package com.project.year2.medicationrecognition;
 
-import android.app.Activity;
-import android.content.ContentResolver;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.util.Log;
-import android.util.SparseArray;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.vision.Frame;
-import com.google.android.gms.vision.text.TextBlock;
-import com.google.android.gms.vision.text.TextRecognizer;
-
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-public class MainActivity extends AppCompatActivity {
-
-    //View in our layout
-
-    ImageView image_view;
-    String mCurrentPhotoPath;
-    private static final int TAKE_PICTURE = 1;
-
-    //will be used to get the image from the device's storage
-    private Uri imageUri;
-
+public class MainActivity extends AppCompatActivity
+{
+    DatabaseHelper myDb;
+    EditText nameText,ingredientText,inventoryText,idText;
+    Button insertButton,displayButton,updateButton,deleteButton;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         image_view = (ImageView) findViewById(R.id.panadolImageView);
+        myDb = new DatabaseHelper(this);
+        //instances of the text box
+        nameText = (EditText)findViewById(R.id.editTextName);
+        ingredientText = (EditText)findViewById(R.id.editTextIngredient);
+        inventoryText = (EditText)findViewById(R.id.editTextInventory);
+        idText = (EditText)findViewById(R.id.editTextID);
+
+        insertButton = (Button)findViewById(R.id.btnInsert);
+        displayButton = (Button)findViewById(R.id.BtnViewData);
+        updateButton = (Button)findViewById(R.id.btnUpdate);
+        deleteButton = (Button)findViewById(R.id.btnDelete);
+
+        addData();
+        //add the function in the main
+       displayData();
+        updateData();
+        deleteData();
 
     }
 
-    public void extractText(Bitmap bitmap) {
+    public void deleteData()
+    {
+        deleteButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Integer deletedRow = myDb.deleteData(idText.getText().toString());
 
-            //Detect text
+                        if (deletedRow > 0)
+                        {
+                            Toast.makeText(MainActivity.this, "Data Deleted", Toast.LENGTH_LONG).show();
 
-            //The Text Recognizer Object Will Detect The Text
-
-            Context context = getApplicationContext();
-
-            TextRecognizer textRecognizer = new TextRecognizer.Builder(context).build();
-
-
-            //check if the text Recognizer object is working before using it to detect text
-
-            if(!textRecognizer.isOperational()){
-
-
-                Log.e("ERROR","DETECTOR DEPENDENCIES ARE NOT AVAILABLE YET");
-
-            }else {
-
-                Toast.makeText(context, "here1", Toast.LENGTH_SHORT).show();
-                // Text Recognizer Is Working
-
-                Frame frame = new Frame.Builder().setBitmap(bitmap).build();
-
-                //Detecting Text
-                //Storing all the results in a sparsearray
-                //sparse array maps integers to objects
-
-                SparseArray<TextBlock> items = textRecognizer.detect(frame);
-
-                Log.i("size",String.valueOf(items.size()));
-
-                for (int i = 0 ; i<items.size();i++){
-
-                    TextBlock item = items.valueAt(i);
-
-                    Log.i("item " + String.valueOf(i),item.getValue());
-
-
-                }
-
-            }
-
-
-    }
-
-
-
-
-    public void openCamera(View view){
-
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        //File will be created in the device public/shared storage , outside the app
-        //this file will contain the image
-        File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
-        //This will allow the full sized imaged to be stored in the file
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
-        imageUri = Uri.fromFile(photo);
-        startActivityForResult(intent, TAKE_PICTURE);
-
-
-    }
-
-
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case TAKE_PICTURE:
-                if (resultCode == Activity.RESULT_OK) {
-                    Uri imageTaken = imageUri;
-                    //This notifies the content resolver that change has happened in the file ( In this case that an image has been saved in the file)
-                    getContentResolver().notifyChange(imageTaken, null);
-                    ContentResolver cr = getContentResolver();
-                    Bitmap bitmap;
-                    //try retrieving the image from teh devices's storage
-                    try {
-                        //This will retrieve the image from the given URI as a bitmap
-                        bitmap = android.provider.MediaStore.Images.Media.getBitmap(cr, imageTaken);
-
-                        image_view.setImageBitmap(bitmap);
-                        // extract text in the image taken by user
-                        extractText(bitmap);
-                    } catch (Exception e) {
-                        Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT)
-                                .show();
-                        Toast.makeText(this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                        }else
+                            Toast.makeText(MainActivity.this, "Data not Deleted", Toast.LENGTH_SHORT).show();
                     }
                 }
-        }
+        );
+    }
+    //update the data in the table
+    public void updateData()
+    {
+        updateButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isUpdated = myDb.updateData(idText.getText().toString(),
+                                nameText.getText().toString(),ingredientText.getText().toString(),inventoryText.getText().toString());
+
+                        if (isUpdated == true)
+                        {
+                            Toast.makeText(MainActivity.this, "Data Updated", Toast.LENGTH_LONG).show();
+
+                        }else
+                            Toast.makeText(MainActivity.this, "Data not Updated", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+        );
     }
 
+//    //Create to display the values function
+    public void displayData()
+    {
+        displayButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Cursor resource = myDb.displayData();
+                //now we will check resources
+                if (resource.getCount()==0)
+                {
+                    showMessage("Error","Nothing found");
+                }else
+                {
+                    StringBuffer buffer = new StringBuffer();
+                    while (resource.moveToNext())
+                    {
+                        buffer.append("Medication ID :" + resource.getString(0)+ "\n");
+                        buffer.append("Name          :" + resource.getString(1)+ "\n");
+                        buffer.append("Ingredient    :" + resource.getString(2)+ "\n");
+                        buffer.append("Inventory     :" + resource.getString(3)+ "\n\n");
+
+
+                    }
+                    showMessage("Medication record",buffer.toString());
+                }
+            }
+
+        });
+    }
+        //Now create the showDialog method
+        public void showMessage(String title,String Message)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setCancelable(true);
+            builder.setTitle(title);
+            builder.setMessage(Message);
+            builder.show();
+        }
+
+        public void addData()
+        {
+            insertButton.setOnClickListener(
+                    new View.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(View v)
+                        {
+                            boolean isInserted = myDb.insertData(nameText.getText().toString(),ingredientText.getText().toString(),inventoryText.getText().toString());
+
+                            if(isInserted)
+                                Toast.makeText(MainActivity.this,"Data Inserted",Toast.LENGTH_LONG).show();
+                            else
+                                Toast.makeText(MainActivity.this, "Data not Inserted", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+            );
+        }
 }
