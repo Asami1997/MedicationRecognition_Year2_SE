361e5200021bb640767f9ad7464cdc3785847ac5
Ahmad Sami Hussein <asami234my@gmail.com>
2017-11-12 17:14:01 +0800

    Creting databse

---
+++ app/src/main/java/com/project/year2/medicationrecognition/Database.java
@@ -0,0 +1,37 @@
+package com.project.year2.medicationrecognition;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by asami on 11/12/2017.
+ */
+
+public class Database {
+
+    Context context;
+
+    public Database() {
+
+        //creating a new database or opening an existing one with mode private
+
+        SQLiteDatabase medicationDatabase = context.openOrCreateDatabase("Medications", Context.MODE_PRIVATE, null);
+
+        //creating a table in the database with 3 columns
+
+        //name and activeIngredient and inventory are the columns
+
+        medicationDatabase.execSQL("CREATE TABLE IF NOT EXISTS medications (name VARCHAR , activeIngredient VARCHAR , inventory INT (3))");
+
+        //inserting medications into table
+
+        medicationDatabase.execSQL("INSERT INTO Medications (name, activeIngredient , inventory) values ('Tritace' ,'ramipri' ,90)");
+
+        medicationDatabase.execSQL("INSERT INTO Medications (name,activeIngredient,inventory) values ('panadol extra' ,'Paracetamol',40)");
+
+        medicationDatabase.execSQL("INSERT INTO Medications (name ,activeIngredient,inventory) values ('Tylenol' ,'Paracetamol' ,0)");
+
+    }
+    
+
+}
