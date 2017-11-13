adb5807b70e8fdddcaa14f9b2f208e034faf49e1
Ahmad Sami Hussein <asami234my@gmail.com>
2017-11-13 17:34:57 +0800

    Updated imageView size

--- app/src/main/java/com/project/year2/medicationrecognition/MainActivity.java
+++ app/src/main/java/com/project/year2/medicationrecognition/MainActivity.java
@@ -9,19 +9,12 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
-
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
     //Initializing pre-processing object
-
-
-
   }
 
   /** Start pick image activity with chooser. */
