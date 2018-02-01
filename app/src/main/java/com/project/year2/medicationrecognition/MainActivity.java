fbec38a5738c820ced32354a98522419993afbaf
Ahmad Sami Hussein <asami1997@gmail.com>
2018-02-02 01:20:11 +0800

    Extraction Updated
    Reuslts are better now . TextBlocks generated by OCR are sorted.

--- app/src/main/java/com/project/year2/medicationrecognition/MainActivity.java
+++ app/src/main/java/com/project/year2/medicationrecognition/MainActivity.java
@@ -18,227 +18,117 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-import opennlp.tools.namefind.NameFinderME;
-import opennlp.tools.namefind.TokenNameFinder;
-import opennlp.tools.namefind.TokenNameFinderModel;
-import opennlp.tools.tokenize.TokenizerME;
-import opennlp.tools.tokenize.TokenizerModel;
-import opennlp.tools.util.Span;
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.regex.*;
-import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.jar.Attributes;
+import java.util.regex.Pattern;
 
-/**
- *
- * In this class , i am trying to find the required information from the generated text , instead of the image itself
- * Using Regular Expressions
- *
- */
 public class MainActivity extends AppCompatActivity {
 
     //View in our layout
 
     ImageView image_view;
-    String mCurrentPhotoPath;
-    String allExtractedText;
     StringBuilder stringBuilder;
-    ArrayList<String> prescriptionDetails;
     private static final int TAKE_PICTURE = 1;
-    private String[] Dictionary;
+    private String[] DATE_Dictionary;
     //will be used to get the image from the device's storage
     private Uri imageUri;
-    private String tempArray[];
-    String newline ;
-    private String splitedString[];
     //This array list will contain the results of the segmentation and searching process
     private ArrayList<String> detailsList;
+    private String test;
+    private String NAME = "";
+    private String GENDER = "";
+    private String BIRTHDATE = "";
+    private String AGE = "";
+    private String PHONE = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         image_view = (ImageView) findViewById(R.id.panadolImageView);
-         //Dictionary array in string.xml file
-         Dictionary = getResources().getStringArray(R.array.Dictionary);
-         newline = System.getProperty("line.separator");
-         detailsList = new ArrayList<String>();
-         stringBuilder = new StringBuilder();
-        naturalLangProcess("Hello I am jhon and i love my mom");
+        image_view = (ImageView) findViewById(R.id.panadolImageView);
+        //Dictionary array in string.xml file
+        DATE_Dictionary = getResources().getStringArray(R.array.dateArray);
+        detailsList = new ArrayList<String>();
+        stringBuilder = new StringBuilder();
+        // naturalLangProcess("Full Patient Name : Jhon ");
     }
 
     public void extractText(Bitmap bitmap) {
 
-            //Detect text
+        //Detect text
 
-            //The Text Recognizer Object Will Detect The Text
+        //The Text Recognizer Object Will Detect The Text
 
-            Context context = getApplicationContext();
+        Context context = getApplicationContext();
 
-            TextRecognizer textRecognizer = new TextRecognizer.Builder(context).build();
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(context).build();
 
+        //check if the text Recognizer object is working before using it to detect text
 
-            //check if the text Recognizer object is working before using it to detect text
-
-            if(!textRecognizer.isOperational()){
-
-
-                Log.e("ERROR","DETECTOR DEPENDENCIES ARE NOT AVAILABLE YET");
-
-            }else {
-
-                Toast.makeText(context, "here1", Toast.LENGTH_SHORT).show();
-                // Text Recognizer Is Working
-
-                Frame frame = new Frame.Builder().setBitmap(bitmap).build();
-
-                //Detecting Text
-                //Storing all the results in a sparsearray
-                //sparse array maps integers to objects
-
-                SparseArray<TextBlock> items = textRecognizer.detect(frame);
-
-                Log.i("size",String.valueOf(items.size()));
-
-                for (int i = 0 ; i<items.size();i++){
-
-                    TextBlock item = items.valueAt(i);
-
-                    String extractedString = item.getValue();
-
-                    Log.i("item " + String.valueOf(i),extractedString);
-
-                    stringBuilder.append(extractedString);
-
-                    /*
-                    //getName(extractedString);
-
-                    for(String value : Dictionary){
-
-                        //extract the first word in the string
-                        tempArray= extractedString.split(" ", 2);
-
-                        //first word in a string
-                        String firstWord = tempArray[0];
+        if (!textRecognizer.isOperational()) {
 
 
-                        Log.i("First word",firstWord);
-                        Log.i("Dictionary value",value);
+            Log.e("ERROR", "DETECTOR DEPENDENCIES ARE NOT AVAILABLE YET");
 
-                        //remove colon from first word
+        } else {
 
-                        //if extracted string contain any word in Dictionary
-                        if(extractedString.contains(value) || extractedString.contains(value.toUpperCase())){
+            Toast.makeText(context, "here1", Toast.LENGTH_SHORT).show();
+            // Text Recognizer Is Working
 
-                         //check if there is only one word
-
-                            Log.i("has first word","yes");
-
-                         if(extractedString.lastIndexOf(value + value.length()) == extractedString.length()-1){
-
-                             //take the next item textRecognizer extracted
-                            Log.i("length","1");
-
-                         }else{
-
-                             //check if there is more than one line
-                             boolean hasNewline = extractedString.contains(newline);
-
-                             if(hasNewline == false){
-
-                                 //has only one line , this is the result
-
-                                 Log.i("one line","yes");
-                                 System.out.println(value + " " + extractedString);
-
-                                 //if already in details arraylist , dont add it
-                                 if(!detailsList.contains(extractedString)){
-
-                                     detailsList.add(extractedString);
-
-                                 }
-
-                             }else{
-                                 //has more than one line
-                                 Log.i("more than one line","yes");
-                                 //check if new lines contain keywords in dictionary
-
-                                 splitedString  = extractedString.split("\\r?\\n");
-
-                                 for(String line : splitedString){
-
-                                     //if line contain keywords
-                                  if(line.contains(value) || line.contains(value.toUpperCase())){
-
-                                      //take what it after it tell end of line
-                                      Log.i("next line value",line);
-                                      //remove this line from extracted string
-                                      extractedString = extractedString.replace(line,"");
-
-                                      Log.i("extracted stringr",extractedString);
-
-                                      if(!detailsList.contains(line)){
-                                          detailsList.add(line);
-                                      }
-
-                                  }else{
-                                      //if line dose not contain keywords
-                                      //add it to result
-                                      Log.i("not added value",line);
-
-                                  }
-
-                                 }
-
-                             }
-                         }
-
-                        }
+            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+            SparseArray<TextBlock> origTextBlocks = textRecognizer.detect(frame);
+            List<TextBlock> textBlocks = new ArrayList<>();
+            for (int i = 0; i < origTextBlocks.size(); i++) {
+                TextBlock textBlock = origTextBlocks.valueAt(i);
+                textBlocks.add(textBlock);
+            }
+            Collections.sort(textBlocks, new Comparator<TextBlock>() {
+                @Override
+                public int compare(TextBlock o1, TextBlock o2) {
+                    int diffOfTops = o1.getBoundingBox().top - o2.getBoundingBox().top;
+                    int diffOfLefts = o1.getBoundingBox().left - o2.getBoundingBox().left;
+                    if (diffOfTops != 0) {
+                        return diffOfTops;
                     }
-                    */
+                    return diffOfLefts;
+                }
+            });
+
+            StringBuilder detectedText = new StringBuilder();
+            for (TextBlock textBlock : textBlocks) {
+                if (textBlock != null && textBlock.getValue() != null) {
+                    detectedText.append(textBlock.getValue());
+                    detectedText.append("\n");
                 }
 
-                allExtractedText = stringBuilder.toString();
-
-                //start the natural language processing
-
-
-                //Log.i("path",String.valueOf(R.raw.ennerperson));
-                Log.i("detailsArrayList",detailsList.toString());
 
             }
-             //naturalLangProcess("Hello I am jhon and i love my mom");
 
-    }
+            Log.i("detected Text", detectedText.toString());
 
-    private void getName(String extractedString) {
-
-        String[] splitedText = extractedString.split("\\r?\\n");
-
-        for(String item : splitedText){
-            Pattern pattern = Pattern.compile("^[a-zA-z ]*$", Pattern.CASE_INSENSITIVE);
-            Matcher matcher = pattern.matcher(item);
-            if (matcher.matches()) {
-                Log.i("This item is a name",item);
-            }else {
-                Log.i("This item is not a name", "No");
-                //loop through dictionary
-            }
+            extactNameAge(detectedText.toString());
+            extractBirthDate(detectedText.toString());
+            extractGender(detectedText.toString());
+            extractPhone(detectedText.toString());
         }
 
+
     }
 
-    public void openCamera(View view){
+    public void openCamera(View view) {
 
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         //File will be created in the device public/shared storage , outside the app
         //this file will contain the image
-        File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
+        File photo = new File(Environment.getExternalStorageDirectory(), "Pic.jpg");
         //This will allow the full sized imaged to be stored in the file
         intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
         imageUri = Uri.fromFile(photo);
@@ -275,47 +165,141 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //extract name using natural language processing
-    public void naturalLangProcess(String text){
 
-        InputStream inputStream = null;
-        inputStream =  getResources().openRawResource(R.raw.ennerperson);
-       // Log.i("path",String.valueOf(R.raw.ennerperson));
+    //look for patient name in the ocr result
+    public void extactNameAge(String ocrResult) {
+        //reset name
+        NAME = "";
+        //reset age
+        AGE = "";
+        //split array by new line
+        String[] splitedArray = ocrResult.split("\\r?\\n");
+        //loop through name dictionary
+        for (int i = 0; i < splitedArray.length - 1; i++) {
+
+            //remove special characters from a string
+            splitedArray[i] = splitedArray[i].replaceAll("[\\-\\+\\^:,]", "");
+
+            Log.i("value", splitedArray[i].toLowerCase());
+
+            //extract name
+            if (Pattern.compile(Pattern.quote("name"), Pattern.CASE_INSENSITIVE).matcher(splitedArray[i].toLowerCase()).find() && NAME.isEmpty()) {
+                int indexOfName = splitedArray[i].toLowerCase().indexOf("name");
+                NAME = splitedArray[i].substring(indexOfName + 4);
+
+                if (NAME.isEmpty()) {
+
+                    NAME = splitedArray[i + 1];
+                }
+                Log.i("valuename", NAME);
+            }
+
+            //extract age
+            if (Pattern.compile(Pattern.quote("age"), Pattern.CASE_INSENSITIVE).matcher(splitedArray[i].toLowerCase()).find() && AGE.isEmpty()) {
+
+                //only if its a digit
+                if (splitedArray[i].substring(3).matches("-?\\d+(\\.\\d+)?")) {
+                    AGE = splitedArray[i].substring(3);
+                }
 
 
-        TokenNameFinderModel model = null;
+                if (AGE.isEmpty()) {
+                    //only if its a digit
+                    if (splitedArray[i + 1].matches("-?\\d+(\\.\\d+)?")) {
+                        AGE = splitedArray[i + 1];
+                    }
+                }
 
-        try {
-            model = new TokenNameFinderModel(inputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
+                Log.i("valueage", AGE);
+            }
+
         }
-
-        NameFinderME nameFinder = new NameFinderME(model);
-        String[] tokens = new String[0];
-        try {
-            tokens = tokenize(text);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Span nameSpans[] = nameFinder.find(tokens);
-        if(nameSpans == null){
-            Log.i("NameSpans ","null");
-        }
-        for(Span s: nameSpans)
-
-
-            Log.i("Iam Name",tokens[s.getStart()]);
-            Log.i("Iam here","yes");
-
-
     }
 
-    public String[] tokenize(String sentence) throws IOException{
-        InputStream inputStreamTokenizer = getResources().openRawResource(R.raw.entoken);
-        TokenizerModel tokenModel = new TokenizerModel(inputStreamTokenizer);
-        TokenizerME tokenizer = new TokenizerME(tokenModel);
-        return tokenizer.tokenize(sentence);
+
+    //look for date of birth
+    public void extractBirthDate(String ocrResult) {
+
+        //reset name
+        BIRTHDATE = "";
+        //split array by new line
+        String[] splitedArray = ocrResult.split("\\r?\\n");
+        //loop through name dictionary
+        for (int i = 0; i < splitedArray.length - 1; i++) {
+
+            //remove special characters from a string
+            splitedArray[i] = splitedArray[i].replaceAll("[\\-\\+\\^:,]", "");
+
+            Log.i("value", splitedArray[i].toLowerCase());
+
+            for (String dicValue : DATE_Dictionary) {
+
+                //extract name
+                if (Pattern.compile(Pattern.quote(dicValue), Pattern.CASE_INSENSITIVE).matcher(splitedArray[i].toLowerCase()).find() && BIRTHDATE.isEmpty()) {
+                    int indexOfName = splitedArray[i].toLowerCase().indexOf(dicValue);
+                    BIRTHDATE = splitedArray[i].substring(dicValue.length());
+
+                    if (BIRTHDATE.isEmpty()) {
+
+                        BIRTHDATE = splitedArray[i + 1];
+                    }
+                    Log.i("valuedate", BIRTHDATE);
+                }
+            }
+
+
+        }
+    }
+
+    //extract gender
+    public void extractGender(String ocrResult) {
+        //reset gender
+        GENDER = "";
+        //split array by new line
+        String[] splitedArray = ocrResult.split("\\r?\\n");
+        //loop through name dictionary
+        for (int i = 0; i < splitedArray.length - 1; i++) {
+
+            if (splitedArray[i].toLowerCase().contains("male")) {
+
+                GENDER = "Male";
+            } else if (splitedArray[i].toLowerCase().contains("female")) {
+                GENDER = "Female";
+            }
+        }
+        Log.i("valuegender", GENDER);
+    }
+
+
+    //extract phone
+    public void extractPhone(String ocrResult) {
+        //reset phone
+        PHONE = "";
+        //split array by new line
+        String[] splitedArray = ocrResult.split("\\r?\\n");
+        //loop through name dictionary
+        for (int i = 0; i < splitedArray.length - 1; i++) {
+
+            //remove special characters from a string
+            splitedArray[i] = splitedArray[i].replaceAll("[\\-\\+\\^:,]", "");
+
+            Log.i("value", splitedArray[i].toLowerCase());
+
+            //extract phone
+            if (Pattern.compile(Pattern.quote("phone"), Pattern.CASE_INSENSITIVE).matcher(splitedArray[i].toLowerCase()).find() && PHONE.isEmpty()) {
+                int indexOfName = splitedArray[i].toLowerCase().indexOf("phone");
+                PHONE = splitedArray[i].substring(indexOfName + 5);
+
+                if (PHONE.isEmpty()) {
+
+                    PHONE = splitedArray[i + 1];
+                }
+                Log.i("valuephone", PHONE);
+            }
+        }
+    }
+
+    public void extractRx(String ocrResult){
+        
     }
 }
